c87fdd34417254630af3b2e7e22cca235a3c63ab
Midokhalil1 <midokhalil22@gmail.com>
2022-11-16 14:02:46 -0500

    adding photo

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+import functions from  ("firebase-functions");
+import express from     "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+app.get('/photos', getAllPhotoa);  
+app.post('/photos', addNewPhoto);   
+
+export const api = functions.http.onRequest(app);
---
+++ src/db.Connect.js
@@ -0,0 +1,7 @@
+import { initializedApp, cert, getApps } from "firebase-admin";
+import { getFirestore } from "firebase-admin/firestore";
+import secrets from "../functions/secrets";
+
+export default function dbConnect() {
+    
+}
